3ab61fff778d298433751b1628c84e044e8c51ed
gtkChop <swaroopbhatk@gmail.com>
2020-03-15 13:14:07 +0000

    added new methods to the main class

--- covid/main.py
+++ covid/main.py
@@ -7,19 +7,65 @@ class CovId19Data(CovIdRequest):
         CovIdRequest.__init__(self, *args, **kwargs)
 
     def get_total_deaths(self):
-        pass
+        """
+        Shows the total death till now
+        Assign: Vipin
+        :return: dict
+        """
 
     def get_total_recovered(self):
+        """
+        Shows total recovered till now
+        Assign: Vipin
+        :return: dict
+        """
         pass
 
     def get_total_confirmed(self):
+        """
+        Shows total confirmed cases till now
+        Assign: Vipin
+        :return: dict
+        """
         pass
 
     def get_total_active(self):
-        pass
+        """
+        Shows total active cases till now
+        Assign: Vipin
+        :return: dict
+        """
 
     def get_new_confirmed(self, period=None):
+        """
+        Periods should be current month or one manth previous data. No old data should be shown
+        :param period:
+        :return:
+        """
         pass
 
     def get_new_recovered(self, period=None):
+        """
+        Periods should be current month or one manth previous data. No old data should be shown
+        :param period:
+        :return:
+        """
+        pass
+
+    def get_all_regional_data(self):
+        pass
+
+    def get_all_country_data(self):
+        pass
+
+    def get_all_records_for_country(self, country=None):
+        pass
+
+    def get_all_records_for_region(self, region=None):
+        pass
+
+    def show_available_countries(self):
+        pass
+
+    def show_available_regions(self):
         pass
